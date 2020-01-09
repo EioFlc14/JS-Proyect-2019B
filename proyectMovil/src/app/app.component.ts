13c78a4cb9f1bcaac78667cda56cec8dc15bdd33
EioFlc14 <edison14_@hotmail.com>
2020-01-09 06:05:40 -0500

    Movil 2

--- proyectMovil/src/app/app.component.ts
+++ proyectMovil/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 
-import { Platform } from '@ionic/angular';
+import {AlertController, Platform} from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
@@ -10,16 +10,39 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
+
 export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    public alertController: AlertController
   ) {
     this.initializeApp();
   }
 
-  initializeApp() {
+  async exitConfirm() {
+    const alert = await this.alertController.create({
+      header: 'Confirmation',
+      message: 'Are you sure that you want to exit',
+      buttons: [
+        {
+          text: 'Exit',
+          handler: () => {
+            // Salir de la aplicacion
+          }
+        }, {
+          text: 'Cancel',
+          handler: () => {
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
